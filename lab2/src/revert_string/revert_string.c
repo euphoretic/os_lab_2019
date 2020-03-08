b8bed7eced8ab425888f7bf6608f6d4cbd0e2097
danil_lans <danimen0001@gmail.com>
2020-03-08 17:39:19 +0000

    all fine

--- lab2/src/revert_string/revert_string.c
+++ lab2/src/revert_string/revert_string.c
@@ -2,6 +2,16 @@
 
 void RevertString(char *str)
 {
-	// your code here
+	int n = 0;
+    int i = 0;
+    for (i = 0; str[i]!= 0; i++)
+    {
+        n++;
+    }
+    for (i = 0; i < n/2; i++)
+    {
+        char between = str[i];
+        str[i] = str[n-i-1];
+        str[n-i-1] = between;
+    }
 }
-
